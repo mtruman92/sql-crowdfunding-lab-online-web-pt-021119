ca19cc97e0798bb06c99b670c7e28b44e3a241c3
Mickae Truman <mtruman@nyit.edu>
2019-04-27 23:45:12 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category, funding goal, start date, end date)
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category, funding_goal, start_date, end_date)
